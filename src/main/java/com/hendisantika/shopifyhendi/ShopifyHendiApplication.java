7f25c8f1fa938ab029e3d2dcaf9e6185bb916a2b
hendisantika <hendisantika@yahoo.co.id>
2023-08-12 13:29:46 +0700

    #1 | Hendi Santika | Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/shopifyhendi/ShopifyHendiApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.shopifyhendi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyHendiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopifyHendiApplication.class, args);
+    }
+
+}
