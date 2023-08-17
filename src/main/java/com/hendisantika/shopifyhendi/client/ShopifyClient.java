010dff2326ea3d326b3d121d385a904f00deb935
hendisantika <hendisantika@yahoo.co.id>
2023-08-17 09:53:07 +0700

    #17 | Hendi Santika | Add getAllProducts in ShopifyClient ✏️:octocat:⬆

--- src/main/java/com/hendisantika/shopifyhendi/client/ShopifyClient.java
+++ src/main/java/com/hendisantika/shopifyhendi/client/ShopifyClient.java
@@ -1,9 +1,11 @@
 package com.hendisantika.shopifyhendi.client;
 
+import com.hendisantika.shopifyhendi.entity.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +23,17 @@ public class ShopifyClient {
 
     @Value("${shopify.app.hostname}")
     private String storeURL;
+
+
+    @Value("${shopify.app.X-Shopify-Access-Token}")
+    private String ShopifyAccessToken;
     private final WebClient client = WebClient.create(storeURL);
+
+    public Flux<Product> getAllProducts() {
+        return client.get()
+                .uri("/admin/api/2023-07/products.json")
+                .header("X-Shopify-Access-Token", ShopifyAccessToken)
+                .exchange().flatMapMany(clientResponse -> clientResponse.bodyToFlux(Product.class)).log("Users Fetched :" +
+                        " ");
+    }
 }
